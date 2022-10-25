29b0d3a817b2894fd9783df3b873009367f75a64
HoangAnh251220 <anhhhhe140734@fpt.edu.vn>
2022-10-25 13:27:15 +0700

    update

---
+++ King and pig ver2/floor_spikes_anim_f3.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.9" tiledversion="1.9.2" name="floor_spikes_anim_f3" tilewidth="16" tileheight="16" tilecount="1" columns="1">
+ <image source="../Tiled2/frames/floor_spikes_anim_f3.png" width="16" height="16"/>
+</tileset>
