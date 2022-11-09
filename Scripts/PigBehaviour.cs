4385b405854ae9b394f4570bb41c6997ecfc8cb1
HoangAnh251220 <anhhhhe140734@fpt.edu.vn>
2022-11-10 00:49:09 +0700

    Add JoyStick MobileApp Version

--- Scripts/PigBehaviour.cs
+++ Scripts/PigBehaviour.cs
@@ -1,84 +1,102 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
 
 public class PigBehaviour : MonoBehaviour
 {
+    public MobileJointStick mobileJointStick;
+    public float playerSpeed;
+    Rigidbody2D rigid;
 
     public bool isDetected = false;
-    Rigidbody2D rigid;
+ 
     public Vector2 speed1 = new Vector2(0, 1f);
     public Vector2 speed2 = new Vector2(1f, 0);
     Vector2 localScale;
     Animator animator;
+
 
     // Start is called before the first frame update
     void Start()
     {
         rigid = this.gameObject.GetComponent<Rigidbody2D>();
         animator = gameObject.GetComponent<Animator>();
+        
     }
 
-    void GoDown(float force)
-    {
-        gameObject.transform.Translate(-speed1 * Time.deltaTime * 2);
-    }
+   
 
-    void GoUp(float force)
-    {
-        gameObject.transform.Translate(speed1 * Time.deltaTime * 2);
-    }
+    //void GoDown(float force)
+    //{
+    //    gameObject.transform.Translate(-speed1 * Time.deltaTime * 2);
+    //}
 
-    void GoRight(float force)
-    {
-        gameObject.transform.Translate(speed2 * Time.deltaTime * 2);
+    //void GoUp(float force)
+    //{
+    //    gameObject.transform.Translate(speed1 * Time.deltaTime * 2);
+    //}
 
-        localScale = transform.localScale;
-        if(localScale.x > 0)
-        {
-            localScale.x *= -1;
-            this.gameObject.transform.localScale = localScale;
-        }
-    }
+    //void GoRight(float force)
+    //{
+    //    gameObject.transform.Translate(speed2 * Time.deltaTime * 2);
 
-    void GoLeft(float force)
-    {
-        gameObject.transform.Translate(-speed2 * Time.deltaTime * 2);
-        if (localScale.x < 0)
-        {
-            localScale.x *= -1;
-            this.gameObject.transform.localScale = localScale;
-        }
-    }
+    //    localScale = transform.localScale;
+    //    if(localScale.x > 0)
+    //    {
+    //        localScale.x *= -1;
+    //        this.gameObject.transform.localScale = localScale;
+    //    }
+    //}
+
+    //void GoLeft(float force)
+    //{
+    //    gameObject.transform.Translate(-speed2 * Time.deltaTime * 2);
+    //    if (localScale.x < 0)
+    //    {
+    //        localScale.x *= -1;
+    //        this.gameObject.transform.localScale = localScale;
+    //    }
+    //}
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        if (Input.GetKey(KeyCode.LeftArrow))
+        if(mobileJointStick.joistickVec.y != 0)
         {
+           
             animator.SetBool("isRun", true);
-            GoLeft(0.5f);
+            rigid.velocity = new Vector2(mobileJointStick.joistickVec.x * playerSpeed, mobileJointStick.joistickVec.y * playerSpeed);       
         }
-        if (Input.GetKey(KeyCode.RightArrow))
+        else
         {
-            animator.SetBool("isRun", true);
-            GoRight(0.5f);
+            rigid.velocity = new Vector2(0, 0);
         }
-        if (Input.GetKey(KeyCode.UpArrow))
-        {
-            animator.SetBool("isRun", true);
-            GoUp(0.5f);
-        }
-        if (Input.GetKey(KeyCode.DownArrow))
-        {
-            animator.SetBool("isRun", true);
-            GoDown(0.5f);
-        }
+        //if (Input.GetKey(KeyCode.LeftArrow))
+        //{
+        //    animator.SetBool("isRun", true);
+        //    GoLeft(0.5f);
+        //}
+        //if (Input.GetKey(KeyCode.RightArrow))
+        //{
+        //    animator.SetBool("isRun", true);
+        //    GoRight(0.5f);
+        //}
+        //if (Input.GetKey(KeyCode.UpArrow))
+        //{
+        //    animator.SetBool("isRun", true);
+        //    GoUp(0.5f);
+        //}
+        //if (Input.GetKey(KeyCode.DownArrow))
+        //{
+        //    animator.SetBool("isRun", true);
+        //    GoDown(0.5f);
+        //}
 
-        if (!Input.anyKey)
-        {
-            animator.SetBool("isRun", false);
-        }
+        //if (!Input.anyKey)
+        //{
+        //    animator.SetBool("isRun", false);
+        //}
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
